5aea90aacfd425d97b86e658b628d174ec2afb88
roch1990 <roch1990@tochka.com>
2017-10-19 22:51:46 +0500

    basic structure with login and dashboard template

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from .database import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(os.environ['APP_SETTINGS'])
+
+    db.init_app(app)
+    with app.test_request_context():
+        db.create_all()
+
+    import app.auth.controllers as auth
+    import app.dashboard.controllers as dashboard
+
+    app.register_blueprint(auth.module)
+    app.register_blueprint(dashboard.module)
+
+    return app
