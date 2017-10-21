d5c61a00921e8794a49ab5e20bc617ec7044516d
Vasiliy Garenskikh <vasya-1995@mail.ru>
2017-10-21 14:10:53 +0500

    WIP github auth

--- app/auth/controllers.py
+++ app/auth/controllers.py
@@ -1,3 +1,4 @@
+import requests
 from flask import (
     Blueprint,
     render_template,
@@ -13,6 +14,8 @@ from sqlalchemy.exc import SQLAlchemyError
 from .models import User, db
 from .forms import UserLoginForm
 
+from .oauth import github
+
 module = Blueprint('auth', __name__, url_prefix ='/')
 
 
@@ -27,3 +30,35 @@ def index():
         return redirect(url_for('dashboard.index'))
     if request.method == 'GET':
         return render_template('auth/login.html')
+
+client_id = github.client_id
+redirect_uri = "http://127.0.0.1:5000/success_github"
+params = {'client_id': client_id,
+          'scope': 'user',
+          'state': 'code123',
+          'redirect_uri': redirect_uri,
+          'allow_signup': "true"}
+
+url = github.get_authorize_url(**params)
+
+@module.route('github_login', methods=['GET'])
+def github_login():
+    return redirect(url)
+
+@module.route('success_github', methods=['GET'])
+def sucess_github():
+    if request.args.get('code'):
+        session = github.get_auth_session(data={'client_id': client_id,
+                                                'client_secret': github.client_secret,
+                                                'code': request.args.get('code'),
+                                                'redirect_uri': redirect_uri,
+                                                'state': 'code123'})
+
+        #TODO
+        '''
+        after implementation of basic auth, we need to check if user no anonymous,
+        if user have account in system we need to login it, and else we need to create new user with
+        credentials from api.github 
+        '''
+
+        return redirect('/')
---
+++ app/auth/oauth.py
@@ -0,0 +1,20 @@
+import os 
+from rauth import OAuth2Service
+
+OAUTH_CREDENTIALS = {
+    'github': {
+        'id': os.environ.get('GITHUB_CLIENT_ID', 'ID'),
+        'secret': os.environ.get('GITHUB_CLIENT_SECRET', 'SECRET')
+    },
+}
+
+github = OAuth2Service(
+    name='github',
+    base_url='https://api.github.com/',
+    authorize_url='https://github.com/login/oauth/authorize',
+    access_token_url='https://github.com/login/oauth/access_token',
+    client_id=OAUTH_CREDENTIALS['github']['id'],
+    client_secret=OAUTH_CREDENTIALS['github']['secret']
+)
+
+
