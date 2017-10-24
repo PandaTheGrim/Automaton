3d3af36ef76f8d0a21f61dd6de5d296bb916aa9d
Vasiliy Garenskikh <vasya-1995@mail.ru>
2017-10-25 01:41:59 +0500

    Add migrate command to manage[dot]py script, remove migrations from .gitignore

--- manage.py
+++ manage.py
@@ -4,10 +4,17 @@ from flask_script import Manager
 
 from app import create_app
 
+from app.database import db
+
+from flask_migrate import Migrate, MigrateCommand
+
 app = create_app()
 app.config.from_object('config.DevelopmentConfig')
 
 manager = Manager(app)
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
